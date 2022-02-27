f094aa5328499105322329ce192ce9db26535664
s3verus <en.severus@gmail.com>
2022-02-28 00:04:46 +0330

    add insert_data(), rename Config struct to Message, edit style and add some TODOs...

--- src/dao.rs
+++ src/dao.rs
@@ -1,18 +1,20 @@
 extern crate redis;
-use serde::{Serialize, Deserialize};
-use std::error::Error;
-use redis::{ControlFlow, PubSubCommands};
 use crate::manager::sanity_check;
+use redis::RedisError;
+use redis::{Commands, ControlFlow, PubSubCommands};
+use serde::{Deserialize, Serialize};
+use std::error::Error;
 
+// what's wrong? can use better form for structures?
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Http {
     optional_resolve_to: Option<String>,
-    address: String
+    address: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Tcp {
-    ip_address: String
+    ip_address: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -22,27 +24,46 @@ pub struct Data {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Config {
-    is_checked: bool,
+pub struct Message {
+    data: Data,
     message: String,
-    data: Data
+    is_checked: bool,
 }
 
-pub fn subscribe(channel: String) -> Result<(), Box<dyn Error>> {
+pub fn psubscribe(channel: String) -> Result<(), Box<dyn Error>> {
+    // TODO read about tokio and async...
     let _ = tokio::spawn(async move {
-    let client = redis::Client::open("redis://127.0.0.1:6379").unwrap();
+        // TODO add logger and handle unwraps
+        // TODO hard code! add config file to project...
+        let client = redis::Client::open("redis://127.0.0.1:6379").unwrap();
+        let mut conn = client.get_connection().unwrap();
 
-    let mut conn = client.get_connection().unwrap();
+        let _: () = conn
+            .psubscribe(&[channel], |msg| {
+                let received: String = msg.get_payload().unwrap();
+                let channel: String = msg.get_channel().unwrap();
+                let message_obj: Message = serde_json::from_str(&received).unwrap();
 
-    let _: () = conn.subscribe(&[channel], |msg| {            
-        let received: String = msg.get_payload().unwrap();
-        let config_obj: Config = serde_json::from_str(&received).unwrap();
+                sanity_check(message_obj, channel);
 
-        sanity_check(config_obj);
-
-        return ControlFlow::Continue;
-    }).unwrap();
+                return ControlFlow::Continue;
+            })
+            .unwrap();
     });
 
+    Ok(())
+}
+
+// insert data to redis with sound: prefix
+pub fn insert_data(message_obj: Message, channel: String) -> Result<(), RedisError> {
+    // connect to redis
+    // TODO hard code!
+    let client = redis::Client::open("redis://127.0.0.1:6379")?;
+    let mut conn = client.get_connection()?;
+
+    let channel: Vec<&str> = channel.split(":").collect();
+    let key = format!("sound:{}", channel[1]);
+    let value = serde_json::to_string(&message_obj).unwrap();
+    conn.set(key, value)?;
     Ok(())
 }
