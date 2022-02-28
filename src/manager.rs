0dc32daab500dcd1060c9315ede51e37954182f8
s3verus <en.severus@gmail.com>
2022-02-28 13:42:47 +0330

    work on sanity_check, i should add some check functions...

--- src/manager.rs
+++ src/manager.rs
@@ -1,7 +1,25 @@
 use crate::dao::insert_data;
 use crate::dao::Message;
 
-pub fn sanity_check(message_obj: Message, channel: String) {
-    println!("{:?}", message_obj);
-    insert_data(message_obj, channel).unwrap();
+pub fn sanity_check(message: Message, channel: String) {
+    if !message.data.tcp.is_none() {
+        let ip_address = match message.data.tcp.as_ref().map(|m| &m.ip_address) {
+            Some(x) => x,
+            _ => "",
+        };
+        println!("{:?}", ip_address);
+    } else if !message.data.http.is_none() {
+        let optional_resolve_to = match message.data.http.as_ref().map(|m| &m.optional_resolve_to) {
+            Some(Some(x)) => x,
+            _ => "",
+        };
+        println!("{:?}", optional_resolve_to);
+        let address = match message.data.http.as_ref().map(|m| &m.address) {
+            Some(x) => x,
+            _ => "",
+        };
+        println!("{:?}", address);
+
+        insert_data(message, channel).unwrap();
+    }
 }
